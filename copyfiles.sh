707b181283009e890bad87c892bb0adcaf7f5a57
Subrata Banik <66223954+Subrata-Banik@users.noreply.github.com>
2024-07-26 12:36:20 +0530

    Add files via upload

---
+++ copyfiles.sh
@@ -0,0 +1,12 @@
+pwd
+# cd /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes
+cd  /Users/animeshrawat/Documents/Code/portfolio-website-dark-light-themes/
+mkdir public
+cp /Users/banik/OneDrive/Documents/dynamicPortfolio/index.html /Users/banik/OneDrive/Documents/dynamicPortfolio/public/
+cp /Users/banik/OneDrive/Documents/dynamicPortfolio/manifest.json /Users/banik/OneDrive/Documents/dynamicPortfolio/public/
+cp /Users/banik/OneDrive/Documents/dynamicPortfolio/serviceWorker.js /Users/banik/OneDrive/Documents/dynamicPortfolio/public/
+cp /Users/banik/OneDrive/Documents/dynamicPortfolio/404.html /Users/banik/OneDrive/Documents/dynamicPortfolio/public/
+cd /Users/banik/OneDrive/Documents/dynamicPortfolio/public/
+mkdir assets
+cp -r /Users/banik/OneDrive/Documents/dynamicPortfolio/assets/* /Users/banik/OneDrive/Documents/dynamicPortfolio/public/assets
+# cp ../index.html ../public/
